1f6107fe78df4bf87622519b1afc71ccb3b2d625
Filipe Belatti <fibelatti@gmail.com>
2018-11-11 21:49:01 +0000

    Downloads lyrics from server

--- app/src/main/kotlin/de/developercity/arcanosradio/features/streaming/data/StreamingDataSource.kt
+++ app/src/main/kotlin/de/developercity/arcanosradio/features/streaming/data/StreamingDataSource.kt
@@ -9,9 +9,14 @@ import de.developercity.arcanosradio.features.streaming.domain.StreamingReposito
 import de.developercity.arcanosradio.features.streaming.domain.models.NowPlaying
 import de.developercity.arcanosradio.features.streaming.domain.models.StreamConfiguration
 import io.reactivex.Single
+import okhttp3.ResponseBody
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Streaming
+import retrofit2.http.Url
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import javax.inject.Inject
 
 class StreamingDataSource @Inject constructor(
@@ -26,6 +31,27 @@ class StreamingDataSource @Inject constructor(
     override fun getCurrentSongMetadata(): Single<NowPlaying> =
         streamingApi.getCurrentSongMetadata(CurrentSongRequestDto())
             .map(currentSongDtoMapper::map)
+            .flatMap { nowPlaying ->
+                getLyrics(nowPlaying.song.lyrics).map {
+                    nowPlaying.copy(song = nowPlaying.song.copy(lyrics = it))
+                }
+            }
+
+    private fun getLyrics(lyricsUrl: String): Single<String> =
+        streamingApi.getSongLyrics(lyricsUrl)
+            .map {
+                val bufferedReader = BufferedReader(InputStreamReader(it.byteStream()))
+                var currentLine = bufferedReader.readLine()
+                val stringBuilder = StringBuilder().append("$currentLine\n")
+
+                while (currentLine != null) {
+                    stringBuilder.append("$currentLine\n")
+                    currentLine = bufferedReader.readLine()
+                }
+                bufferedReader.close()
+
+                stringBuilder.toString()
+            }
 }
 
 interface StreamingApi {
@@ -34,4 +60,8 @@ interface StreamingApi {
 
     @POST("/parse/classes/Playlist")
     fun getCurrentSongMetadata(@Body currentSongRequestDto: CurrentSongRequestDto): Single<CurrentSongDto>
+
+    @Streaming
+    @GET
+    fun getSongLyrics(@Url lyricsUrl: String): Single<ResponseBody>
 }
